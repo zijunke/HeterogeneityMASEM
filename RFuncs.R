c1e28b128904a9e6c74311d27f35eb3961dd4d04
zijunke <39897877+zijunke@users.noreply.github.com>
2020-05-28 14:22:33 +0800

    Update RFuncs.R

--- RFuncs.R
+++ RFuncs.R
@@ -143,7 +143,7 @@ wbugs <-function(data,initsl,prm,mfn,prm.del.conv,
 	for(tryi in 2:20){
 		print(paste0('Iteration: ',tryi*(niter-nburnin)))
 		fit.coda = read.openbugs(stem="",thin = nthin,quiet=T)
-		del.id = na.omit(match(c('ppp',prm.conv),varnames(fit.coda)))
+		del.id = na.omit(match(c('ppp',prm.del.conv),varnames(fit.coda)))
 		#print(summary(fit.coda),3)
 		if(diagm=='Geweke'){
 			if(length(del.id)>0){
